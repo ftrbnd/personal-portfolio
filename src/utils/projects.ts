1bffc225b815bae6f4f0d53e4cf410f26cede0e7
Giovanni Salas <46335023+ftrbnd@users.noreply.github.com>
2023-07-11 15:54:41 -0700

    Update projects

--- src/utils/projects.ts
+++ src/utils/projects.ts
@@ -9,44 +9,44 @@ export interface Project {
 export const projects: Project[] = [
   {
     name: 'EDEN Heardle',
-    description: 'The audio-based version of Wordle, specifically made for songs from the artist known as EDEN.',
+    description: 'The audio-based version of Wordle, specifically made for songs from the musical artist EDEN. Created with TypeScript, React, Redux, Firebase, React Router, and Material UI.',
     thumbnailUrl: 'https://i.imgur.com/hJda2lu.png',
     repoUrl: 'https://github.com/ftrbnd/odin-js-final-project',
     demoUrl: 'https://eden-heardle.web.app'
   },
   {
     name: 'EDEN Discord Bot',
-    description: 'A Discord bot made for a community server of over 3,000 members',
+    description: 'A Discord bot made for a community server of over 3,000 members! Includes moderation features, birthday reminders, a "Guess the Song" game, and more.',
     thumbnailUrl: 'https://i.imgur.com/sj4kU3x.png',
     repoUrl: 'https://github.com/ftrbnd/eden-bot-v13',
     demoUrl: 'https://discord.gg/futurebound'
   },
   {
-    name: 'EDEN Heardle',
-    description: 'The audio-based version of Wordle, specifically made for songs from the artist known as EDEN.',
-    thumbnailUrl: 'https://i.imgur.com/hJda2lu.png',
-    repoUrl: 'https://github.com/ftrbnd/odin-js-final-project',
-    demoUrl: 'https://eden-heardle.web.app'
+    name: '',
+    description: '',
+    thumbnailUrl: '',
+    repoUrl: '',
+    demoUrl: ''
   },
   {
-    name: 'EDEN Discord Bot',
-    description: 'A Discord bot made for a community server of over 3,000 members',
-    thumbnailUrl: 'https://i.imgur.com/sj4kU3x.png',
-    repoUrl: 'https://github.com/ftrbnd/eden-bot-v13',
-    demoUrl: 'https://discord.gg/futurebound'
+    name: '',
+    description: '',
+    thumbnailUrl: '',
+    repoUrl: '',
+    demoUrl: ''
   },
   {
-    name: 'EDEN Heardle',
-    description: 'The audio-based version of Wordle, specifically made for songs from the artist known as EDEN.',
-    thumbnailUrl: 'https://i.imgur.com/hJda2lu.png',
-    repoUrl: 'https://github.com/ftrbnd/odin-js-final-project',
-    demoUrl: 'https://eden-heardle.web.app'
+    name: '',
+    description: '',
+    thumbnailUrl: '',
+    repoUrl: '',
+    demoUrl: ''
   },
   {
-    name: 'EDEN Discord Bot',
-    description: 'A Discord bot made for a community server of over 3,000 members',
-    thumbnailUrl: 'https://i.imgur.com/sj4kU3x.png',
-    repoUrl: 'https://github.com/ftrbnd/eden-bot-v13',
-    demoUrl: 'https://discord.gg/futurebound'
+    name: '',
+    description: '',
+    thumbnailUrl: '',
+    repoUrl: '',
+    demoUrl: ''
   }
 ];
